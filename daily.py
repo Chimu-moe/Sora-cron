d1f51e5023d61c909948d60596420750a76290ea
Robin A. P <me@mempler.de>
2020-05-26 06:13:38 +0200

    perhabs a fix

--- daily.py
+++ daily.py
@@ -10,10 +10,15 @@ except:
 # Get current position for the current day. this cron runs every day ONCE!
 users = SQL.query("SELECT Id FROM Users")
 for user in users:
-    user_lb = SQL.query("SELECT * FROM Leaderboard WHERE id = %s LIMIT 1", (user['Id']))
-    
+    user_lb = SQL.query("SELECT * FROM Leaderboard WHERE OwnerId = %s LIMIT 1", (user['Id']))
+    if (len(user_lb) == 0):
+        continue
+
+    user_lb = user_lb[0]
+
     for mode in [0,1,2,3]:
         key = f"sora:performance:{user['Id']}_{mode}"
+        print(key)
         last = REDIS.get(key)
         if last == None:
             last = []
@@ -26,13 +31,13 @@ for user in users:
         position = 0
         
         if mode == 0:
-            position = SQL.query("SELECT COUNT(*) as position FROM Leaderboard WHERE Id = %s AND PerformancePointsOsu > PerformancePointsOsu", user['Id'])[0]['position']
+            position = SQL.query("SELECT COUNT(%s > PerformancePointsOsu) as position FROM Leaderboard WHERE OwnerId != %s", (user['Id'], user_lb['PerformancePointsOsu']))[0]['position']
         elif mode == 1:
-            position = SQL.query("SELECT COUNT(*) as position FROM Leaderboard WHERE Id = %s AND PerformancePointsOsu > PerformancePointsTaiko", user['Id'])[0]['position']
+            position = SQL.query("SELECT COUNT(%s > PerformancePointsTaiko) as position FROM Leaderboard WHERE OwnerId != %s", (user['Id'], user_lb['PerformancePointsOsu']))[0]['position']
         elif mode == 2:
-            position = SQL.query("SELECT COUNT(*) as position FROM Leaderboard WHERE Id = %s AND PerformancePointsOsu > PerformancePointsCtb", user['Id'])[0]['position']
+            position = SQL.query("SELECT COUNT(%s > PerformancePointsCtb) as position FROM Leaderboard WHERE OwnerId != %s", (user['Id'], user_lb['PerformancePointsOsu']))[0]['position']
         elif mode == 3:
-            position = SQL.query("SELECT COUNT(*) as position FROM Leaderboard WHERE Id = %s AND PerformancePointsOsu > PerformancePointsMania", user['Id'])[0]['position']
+            position = SQL.query("SELECT COUNT(%s > PerformancePointsMania) as position FROM Leaderboard WHERE OwnerId != %s", (user['Id'], user_lb['PerformancePointsOsu']))[0]['position']
 
         last.append(position) # Next Position
         REDIS.set(key, json.dumps(last))
